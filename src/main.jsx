90cedef87d592d7b908adf8adb6d730972bd957a
Iamywh <165839947+Iamywh@users.noreply.github.com>
2026-08-09 20:46:49 +0100

    Load premium theme and popup styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
 import './styles/footer.css'
+import './styles/premium-theme.css'
+import './styles/popups.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
